993fc3f7c028b52cba7b322bd6ce801bdbaaba56
Ben Christensen <ben@joinhandshake.com>
2024-07-13 10:39:02 -0700

    Notes for next, and switch to storing rounds in a slice

--- Readme.md
+++ Readme.md
@@ -1,2 +1,35 @@
 Run GraphQL server: go run main.go
 Run frontend - yarn start in client-app subdir
+
+A game has players and rounds
+In a round each player has a bid and a score
+
+Game
+- Id
+- DateTime
+- Has many PlayerRounds
+- Has many Players (not re-used outside of games)
+
+PlayerRounds
+- ID: Round+Player
+- Round (-10 to -1, 1 to 10)
+- Bid
+- Score
+- Player (string name as the ID, can later be joined to some table)
+- Game
+
+
+TODO
+- Render them
+- Have proper ID structure for PlayerRound
+- Should we build all rounds at game start, or build them as we go?
+ - Probably add as we go
+- Allow mutating a given playerround's score and bid
+
+- Switch to a persistent data store
+- Add "Game" scoping (instead of player rounds being global)
+
+
+Ideas
+- Button to hit when player takes a trick
+-
--- gql-server/database.go
+++ gql-server/database.go
@@ -1,4 +1,6 @@
 package main
+
+import ("math/rand/v2")
 // put this into its own package later
 
 type PlayerRound struct {
@@ -8,25 +10,26 @@ type PlayerRound struct {
   Score   string  `json:"text"`
 }
 
-// Mock data
-var round1 = &PlayerRound{
-  Id: "1",
-  Player: "Ben",
-  Bid: "2",
-  Score: "1",
-}
-var round2 = &PlayerRound{
-  Id: "2",
-  Player: "Scott",
-  Bid: "5",
-  Score: "0",
+var rounds = []PlayerRound{
+  PlayerRound{
+    Id: "1",
+    Player: "Ben",
+    Bid: "2",
+    Score: "1",
+  },
+  PlayerRound{
+    Id: "2",
+    Player: "Scott",
+    Bid: "5",
+    Score: "0",
+  },
 }
 
 func GetAllRounds() []PlayerRound {
-  return []PlayerRound{*round1, *round2}
+  return rounds;
 }
 
 // Temporary, for mocking out
-func GetRandomRound() *PlayerRound {
-  return round1
+func GetRandomRound() PlayerRound {
+  return rounds[rand.IntN(len(rounds))];
 }
